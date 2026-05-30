3b98bca4bbd5db706b320aed2f5d37b50ebd9093
Leonie Hoffmann <265492205+leoniehff13-blip@users.noreply.github.com>
2026-05-30 17:59:25 +0200

    fix: Appwrite config als Fallback hardcoden

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -1,8 +1,8 @@
 import { Client, Databases, ID, Query } from "appwrite";
 
 const client = new Client()
-  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
-  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
+  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT || "https://fra.cloud.appwrite.io/v1")
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID || "6a1ab06700390e26dfcb");
 
 export const databases = new Databases(client);
 export { ID, Query };
